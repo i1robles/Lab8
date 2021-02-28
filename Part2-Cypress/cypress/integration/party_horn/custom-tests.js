28c7250dc675629428b5b483247609c273250ca8
Ivan Robles <i1robles@ucsd.edu>
2021-02-27 17:25:13 -0800

    initialized, added first test

---
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -0,0 +1,19 @@
+describe('Party Horn Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/Part2-Cypress/index.html');
+  });
+
+  it('First Test', () => {
+    expect(true).to.equal(true);
+  });
+
+  it('Slider changes when volume input changes', () => {
+    cy.get('#volume-number').clear().type('75');
+    cy.get('#volume-slider')
+    .then(function testVS($el) {
+      expect($el).to.have.value(75);
+    })
+
+
+  });
+});
